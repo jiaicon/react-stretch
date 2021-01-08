f27520dc8efda9807efb1917328b1373e9d1286c
jiaxiangting <jiaxiangting@socmap.net>
2021-01-08 15:30:38 +0800

    图片大小loader

--- src/app.tsx
+++ src/app.tsx
@@ -3,5 +3,10 @@ import ReactDOM from 'react-dom';
 
 // import {UploadPhoto} from './main';
 import Stretch from './Stretch';
+import Test from './../dist/index';
+import './../dist/css/main.988af2.css';
 
-ReactDOM.render(<Stretch/>, document.getElementById('app'));
+ReactDOM.render(<div>
+    <Stretch/>
+    <Test/>
+</div>, document.getElementById('app'));
