631fc94095e64f48adb6b2592e7213fe355118a2
jiaxiangting <jiaxiangting@socmap.net>
2021-01-08 16:31:53 +0800

    发包

--- src/app.tsx
+++ src/app.tsx
@@ -3,10 +3,5 @@ import ReactDOM from 'react-dom';
 
 // import {UploadPhoto} from './main';
 import Stretch from './Stretch';
-import Test from './../dist/index';
-import './../dist/css/main.988af2.css';
 
-ReactDOM.render(<div>
-    <Stretch/>
-    <Test/>
-</div>, document.getElementById('app'));
+ReactDOM.render(<Stretch/>, document.getElementById('app'));
